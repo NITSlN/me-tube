a5eaaa1e86bcb040e75f1992aeff7f4e8c95084e
Nitish Sinha <76836759+NITSlN@users.noreply.github.com>
2022-08-02 13:35:12 +0530

    baseURL axios

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import axios from 'axios'
 import { LoginProvider } from './context/LoginContext'
-axios.defaults.baseURL = 'https://me-tube-backend.herokuapp.com/api';
+axios.defaults.baseURL = 'http://localhost:3001/api';
+axios.defaults.withCredentials=true
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <LoginProvider>
       <App />
   </LoginProvider>,
 )
+
